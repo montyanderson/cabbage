b3b6075d6e419c66dd534bc45866e63d08a432ce
Monty Anderson <monty.linux@gmail.com>
2017-08-08 11:41:56 +0100

    validates github push secret

--- routes/push.js
+++ routes/push.js
@@ -1,10 +1,19 @@
 const _ = require("koa-route");
+const crypto = require("crypto");
 const fs = require("mz/fs");
 const Project = require("../lib/Project");
 
 const secret = fs.readFileSync(__dirname + "/../.push_secret", "utf8").trim();
 
 module.exports = _.post("/push", async ctx => {
+	const hmac = crypto.createHmac("sha1", secret);
+	hmac.update(ctx.request.rawBody);
+
+	if(`sha1=${hmac.digest("hex")}` !== ctx.request.headers["x-hub-signature"]) {
+		ctx.response.status = 401;
+		return;
+	}
+
 	const project = await Project.findByRepo(ctx.request.body.repository.full_name);
 
 	if(project.active == true) {
