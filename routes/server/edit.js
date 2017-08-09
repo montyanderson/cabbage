54bffc8ecaa12603fe40bd7fb4a13d0d3cf5a298
Monty Anderson <montydev@protonmail.com>
2017-08-09 12:22:55 +0100

    fixed /server/edit endpoint

--- routes/server/edit.js
+++ routes/server/edit.js
@@ -1,7 +1,7 @@
 const _ = require("koa-route");
 const Server = require("../../lib/Server");
 
-module.exports = _.post("/server/edit", async ctx => {
+module.exports = _.post("/server", async ctx => {
 	const server = new Server(ctx.request.body);
 
 	await server.save();
