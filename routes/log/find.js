e2c9c4901d8f5ee8da3afa19c69f0a6323c5ca13
Monty Anderson <montydev@protonmail.com>
2017-11-03 11:40:45 +0000

    x

--- routes/log/find.js
+++ routes/log/find.js
@@ -2,7 +2,5 @@ const _ = require("koa-route");
 const log = require("../../lib/Log");
 
 module.exports = _.get("/log", async ctx => {
-	const log = await Log.find(ctx.query.id);
-
-	ctx.body = log;
+	ctx.body = await Log.find(ctx.query.id);
 });
